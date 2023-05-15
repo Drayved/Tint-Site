68cf17d37b2b862534778e3876e2531076dc405b
David Riley <Dril112191@gmail.com>
2023-05-15 16:16:21 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,21 @@
-import React from "react"
+import React from "react";
 
-export default function Footer(){
-    const handleBackToTop = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      };
-    
+export default function Footer() {
+  const handleBackToTop = () => {
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, 100);
+  };
 
-    return(
-        <div className="footer-container">
-            <div className="footer-btn-container">
-                <button className="footer-btn" onClick={handleBackToTop}>
-                    BACK TO TOP
-                </button>
-            </div>
-            <h4 className="footer-phone">(303) 803-5843 <span className="footer-email">EMAIL@EMAIL.COM</span></h4>
-            <h4 className="footer-name">©2019 Your Company Name Here</h4>
-        </div>
-    )
+  return (
+    <div className="footer-container">
+      <div className="footer-btn-container">
+        <button className="footer-btn" onClick={handleBackToTop}>
+          BACK TO TOP
+        </button>
+      </div>
+      <h4 className="footer-phone">(303) 803-5843 <span className="footer-email">EMAIL@EMAIL.COM</span></h4>
+      <h4 className="footer-name">©2019 Your Company Name Here</h4>
+    </div>
+  );
 }
