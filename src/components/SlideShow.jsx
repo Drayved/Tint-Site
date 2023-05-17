ec8fe94d0f612fa30db89626f897e53a30a1b4d6
David Riley <Dril112191@gmail.com>
2023-05-17 17:07:42 -0400

    .

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-export default function SlideShow({ images }) {
+export default function SlideShow({images}) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [touchStartX, setTouchStartX] = useState(null);
-  const [swipeDirection, setSwipeDirection] = useState('');
 
   const previousImage = () => {
     const index = (currentIndex - 1 + images.length) % images.length;
@@ -15,29 +13,6 @@ export default function SlideShow({ images }) {
     setCurrentIndex(index);
   };
 
-  const handleTouchStart = (e) => {
-    setTouchStartX(e.touches[0].clientX);
-    setSwipeDirection('');
-  };
-
-  const handleTouchMove = (e) => {
-    if (touchStartX !== null) {
-      const touchCurrentX = e.touches[0].clientX;
-      const diffX = touchStartX - touchCurrentX;
-      const threshold = 50;
-
-      if (diffX > threshold) {
-        setSwipeDirection('left');
-      } else if (diffX < -threshold) {
-        setSwipeDirection('right');
-      } else {
-        setSwipeDirection('');
-      }
-
-      setTouchStartX(null);
-    }
-  };
-
   // Automatically switch to the next image every 3 seconds
   useEffect(() => {
     const interval = setInterval(() => {
@@ -47,19 +22,7 @@ export default function SlideShow({ images }) {
   }, [currentIndex]);
 
   return (
-    <div
-      className="slideshow-container"
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={() => {
-        setTouchStartX(null);
-      }}
-    >
-      {swipeDirection && (
-        <div className="swipe-indicator">
-          Swipe {swipeDirection === 'left' ? 'left' : 'right'}
-        </div>
-      )}
+    <div className="slideshow-container">
       <div className="slideshow">
         <img
           src={images[currentIndex]}
@@ -70,20 +33,26 @@ export default function SlideShow({ images }) {
           {images.map((image, index) => (
             <div
               key={index}
-              className={currentIndex === index ? 'bubble active' : 'bubble'}
+              className={currentIndex === index ? "bubble active" : "bubble"}
               onClick={() => setCurrentIndex(index)}
             ></div>
           ))}
         </div>
         <div className="arrow-container">
-          <button onClick={previousImage} className="arrows">
+          <button
+            onClick={previousImage}
+            className="arrows"
+          >
             &#8249;
           </button>
-          <button onClick={nextImage} className="arrows">
+          <button
+            onClick={nextImage}
+            className="arrows"
+          >
             &#8250;
           </button>
         </div>
       </div>
     </div>
   );
-}
+};
