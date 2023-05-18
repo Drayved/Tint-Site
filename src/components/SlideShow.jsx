89bf1f4ca679f699b20c06022dcfbdb0325ace53
David Riley <Dril112191@gmail.com>
2023-05-18 12:08:38 -0400

    swipe function for mobile

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,58 +1,108 @@
 import React, { useState, useEffect } from 'react';
+import SwipeableViews from 'react-swipeable-views';
 
-export default function SlideShow({images}) {
+export default function SlideShow({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const previousImage = () => {
-    const index = (currentIndex - 1 + images.length) % images.length;
-    setCurrentIndex(index);
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        return images.length - 1; // Loop to the last image
+      }
+      return prevIndex - 1;
+    });
   };
 
   const nextImage = () => {
-    const index = (currentIndex + 1) % images.length;
-    setCurrentIndex(index);
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex === images.length - 1) {
+        return 0; // Loop to the first image
+      }
+      return prevIndex + 1;
+    });
   };
 
-  // Automatically switch to the next image every 3 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       nextImage();
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [currentIndex]);
+    }, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const handleSwipe = (index) => {
+    if (index === images.length + 1) {
+      // Reached the cloned last image, loop to the first image
+      setCurrentIndex(0);
+    } else if (index === 0) {
+      // Reached the cloned first image, loop to the last image
+      setCurrentIndex(images.length - 1);
+    } else {
+      setCurrentIndex(index - 1);
+    }
+  };
+
+  const renderImages = () => {
+    // Clone the first and last images to create a seamless circular loop
+    const clonedImages = [images[images.length - 1], ...images, images[0]];
+
+    return clonedImages.map((image, index) => (
+      
+      <div key={index} className="slide">
+        
+        <img src={image} alt="Slide" className="current-img" />
+        <div className="img-film">{renderBubbles()}</div>
+      </div>
+    ));
+  };
+
+  const renderBubbles = () => {
+    // Exclude the cloned images when rendering the bubbles
+    return images.map((_, index) => (
+      <div
+        key={index}
+        className={currentIndex === index ? 'bubble active' : 'bubble'}
+        onClick={() => setCurrentIndex(index)}
+      />
+    ));
+  };
 
   return (
     <div className="slideshow-container">
-      <div className="slideshow">
-        <img
-          src={images[currentIndex]}
-          alt="Slide"
-          className="current-img"
-        />
-        <div className="img-film">
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className={currentIndex === index ? "bubble active" : "bubble"}
-              onClick={() => setCurrentIndex(index)}
-            ></div>
-          ))}
-        </div>
-        <div className="arrow-container">
-          <button
-            onClick={previousImage}
-            className="arrows"
+      <div className={window.innerWidth > 600 ? 'slideshow' : ''}>
+        {window.innerWidth > 600 ? (
+          // Large screens: use arrow buttons
+          <>
+            <img
+              src={images[currentIndex]}
+              alt="Slide"
+              className="current-img"
+            />
+            <div className="img-film">{renderBubbles()}</div>
+            <div className="arrow-container">
+              <button onClick={previousImage} className="arrows">
+                &#8249;
+              </button>
+              <button onClick={nextImage} className="arrows">
+                &#8250;
+              </button>
+            </div>
+          </>
+        ) : (
+          // Small screens: use swipeable views
+          <SwipeableViews
+            index={currentIndex + 1}
+            onChangeIndex={handleSwipe}
+            resistance
+            resistanceFactor={0.6}
           >
-            &#8249;
-          </button>
-          <button
-            onClick={nextImage}
-            className="arrows"
-          >
-            &#8250;
-          </button>
-        </div>
+            
+            {renderImages()}
+          </SwipeableViews>
+        )}
       </div>
     </div>
   );
-};
+}
